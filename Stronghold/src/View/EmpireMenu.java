85d8cb715c04311059eeb31198c1086125dec7cf
mohammad.h <m.h.b.b2045@gmail.com>
2023-04-21 18:47:52 +0330

    finish -> shop menu & empire menu | in progress -> building type

--- Stronghold/src/View/EmpireMenu.java
+++ Stronghold/src/View/EmpireMenu.java
@@ -1,6 +1,7 @@
 package View;
 
 import Controller.EmpireMenuController;
+import Enums.EmpireMenuCommands;
 
 import java.util.regex.Matcher;
 
@@ -22,15 +23,15 @@ public class EmpireMenu {
                 System.out.println(empireMenuController.showPopularity());
             } else if (command.matches("^show food list$")) {
                 System.out.println(empireMenuController.showFoodList());
-            } else if ((matcher = Menu.getMatcher(command, "food rate -r (?<rate>-?\\d+)")) != null) {
+            } else if ((matcher = EmpireMenuCommands.getMatcher(command, EmpireMenuCommands.FOOD_RATE_REGEX)) != null) {
                 empireMenuController.changeFoodRate(matcher);
             } else if (command.matches("^food rate show$")) {
                 System.out.println(empireMenuController.foodRateShow());
-            } else if ((matcher = Menu.getMatcher(command, "tax rate -r (?<rate>-?\\d+)")) != null) {
+            } else if ((matcher = EmpireMenuCommands.getMatcher(command, EmpireMenuCommands.TAX_RATE_REGEX)) != null) {
                 empireMenuController.changeTaxRate(matcher);
             } else if (command.matches("^tax rate show$")) {
                 System.out.println(empireMenuController.taxRateShow());
-            } else if ((matcher = Menu.getMatcher(command, "fear rate -r (?<rate>-?\\d+)")) != null) {
+            } else if ((matcher = EmpireMenuCommands.getMatcher(command, EmpireMenuCommands.FEAR_RATE_REGEX)) != null) {
                 empireMenuController.changeFearRate(matcher);
             } else if (command.matches("^fear rate show$")) {
                 System.out.println(empireMenuController.fearRateShow());
